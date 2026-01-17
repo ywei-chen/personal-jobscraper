fca21761588059e6761d325f396aedeb90b5c9bf
YiWei <b85125@gmail.com>
2026-01-17 22:18:40 +0800

    feat(etl): modify data logic

--- ETL_pipline/Transform.py
+++ ETL_pipline/Transform.py
@@ -15,18 +15,22 @@ def transform_jobData(jobData):
             'company': job.get('custName', None),
             'name': job.get('jobName', None),
             'addr': job.get('jobAddrNoDesc', None),
-            'salary': f"{job.get('salaryLow')} ~ {job.get('salaryHigh')}"
-                          if job.get('salaryLow') and job.get('salaryHigh')
-                          else "待遇面議",
+            'salary': (
+                f"{job.get('salaryLow')}元以上"
+                if job.get('salaryHigh') == 9999999 and job.get('salaryLow')
+                else f"{job.get('salaryLow')}元 ~ {job.get('salaryHigh')}元"
+                if job.get('salaryLow') and job.get('salaryHigh')
+                else "待遇面議"
+            ),
                           
             # 點擊詳情可顯示: 公司名稱、職稱、具體地址、薪資、工作內容、擅長工具、上班時間、應徵連結
             'description': job.get('description', None),
-            'realAddr': f"{job.get('jobAddrNoDesc')}{job.get('jobAddress')}",
+            'realAddr': (f"{job.get('jobAddrNoDesc')}{job.get('jobAddress')}"),
             'workingtime': job.get('d3'),
-            'skill': '、'.join([s['description'] for s in job.get('pcSkills')]) 
+            'skill': ('、'.join([s['description'] for s in job.get('pcSkills')]) 
                      if job.get('pcSkills') 
-                     else "不拘",
-            'link': job.get('link', {}).get('job', None)
+                     else "不拘"),
+            'link': (job.get('link', {}).get('job', None))
         }
         
         transformed_jobData.append(each_jobData)
